60cc6085d3631fad6f2518786a3e44402dcf9326
jugosx <priskillacantix@gmail.com>
2019-05-31 14:10:31 +0700

    Add graphiql to server

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql')
 const { buildSchema } = require('graphql');
 
-var app = express()
-
+const app = express()
+let schema = buildSchema(`
+    type Query {
+      hello: String
+    }
+  `)
+let root = {
+  hello: () => 'hello world!!!'
+}
+app.use('/graphql',graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}))
 app.listen(4000, ()=> console.log('berhasil berjalan'))
