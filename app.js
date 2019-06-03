163983df00faf43841d8213360b6536f8bb4f233
jugosx <priskillacantix@gmail.com>
2019-06-03 21:49:37 +0700

    [Add] Data Source, List Data

--- app.js
+++ app.js
@@ -3,17 +3,29 @@ const graphqlHTTP = require('express-graphql')
 const { buildSchema } = require('graphql');
 
 const app = express()
+
+//Sumber Data
+let forumData = [
+  {id: "1", title:"Cara Belajar", desc:"Bagaimana cara belajar yang baik"},
+  {id: "2", title:"Apa Sekarang", desc:"Sekarang harus belajar"},
+  {id: "3", title:"Mulai Dari Mana", desc:"Saya Bingung Mulai Dari mana"},
+]
 let schema = buildSchema(`
+    type Forum {
+      id: ID,
+      title: String,
+      desc: String
+    }
     type Query {
-      hello: String
+      forums: [Forum]
     }
   `)
-let root = {
-  hello: () => 'hello world!!!'
+let resolvers = {
+  forums: () => forumData
 }
 app.use('/graphql',graphqlHTTP({
     schema: schema,
-    rootValue: root,
+    rootValue: resolvers,
     graphiql: true
 }))
 app.listen(4000, ()=> console.log('berhasil berjalan'))
