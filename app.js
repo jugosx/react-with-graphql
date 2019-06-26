af49bf36287432eb0716b22e808276d1d0365630
jugosx <priskillacantix@gmail.com>
2019-06-26 11:34:20 +0700

    Create Datalist

--- app.js
+++ app.js
@@ -6,10 +6,18 @@ const app = express()
 
 //Sumber Data
 let forumData = [
-  {id: "1", title:"Cara Belajar", desc:"Bagaimana cara belajar yang baik"},
-  {id: "2", title:"Apa Sekarang", desc:"Sekarang harus belajar"},
-  {id: "3", title:"Mulai Dari Mana", desc:"Saya Bingung Mulai Dari mana"},
+  {id: "1", title:"Cara Belajar", desc:"Bagaimana cara belajar yang baik", userId: '1'},
+  {id: "2", title:"Apa Sekarang", desc:"Sekarang harus belajar", userId: '2'},
+  {id: "3", title:"Mulai Dari Mana", desc:"Saya Bingung Mulai Dari mana", userId: '1'},
 ]
+
+const userData = [
+  {id: '1', name: 'naruto'},
+  {id: '2', name: 'sasuke'},
+]
+
+
+
 let schema = buildSchema(`
     type Forum {
       id: ID,
@@ -17,10 +25,14 @@ let schema = buildSchema(`
       desc: String
     }
     type Query {
+      forum(id: ID!): Forum,
       forums: [Forum]
     }
   `)
 let resolvers = {
+  forum: (args) => {
+    return forumData.find(el => el.id == args.id)
+  },
   forums: () => forumData
 }
 app.use('/graphql',graphqlHTTP({
